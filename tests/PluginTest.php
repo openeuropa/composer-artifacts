d20901115d48b9b5596973e7572ce1cc16e5c88b
Antonio De Marco <antonio@nuvole.org>
2018-05-22 12:06:36 +0200

    OPENEUROPA-575: Initial test.

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -2,6 +2,10 @@
 
 namespace OpenEuropa\ComposerArtifacts\Tests;
 
+use Composer\Composer;
+use Composer\IO\NullIO;
+use Composer\Package\RootPackage;
+use OpenEuropa\ComposerArtifacts\Plugin;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -11,10 +15,48 @@ class PluginTest extends TestCase
 {
 
     /**
-    * Smoke test.
-    */
-    public function testSmoke()
+     * Test artifact.
+     */
+    public function testSetArtifactDist()
     {
-        $this->assertTrue(true);
+        $package = new RootPackage('test/test', '1.0.0', '1.0.0');
+        $package->setExtra([
+            'artifacts' => [
+                'test/test' => [
+                    'dist' => [
+                        'url' => '{version}.tar.gz',
+                        'type' => 'tar',
+                    ]
+                ]
+            ]
+        ]);
+        $io = new NullIO();
+        $composer = new Composer();
+        $composer->setPackage($package);
+
+        $plugin = new Plugin();
+        $plugin->activate($composer, $io);
+
+        $this->invokeMethod($plugin, 'setArtifactDist', [$package]);
+        $this->assertEquals('1.0.0.tar.gz', $package->getDistUrl());
+    }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param $object
+     * @param $methodName
+     * @param array      $parameters
+     *
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
     }
 }
